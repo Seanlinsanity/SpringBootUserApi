a509d46927445beedda638fe515a664ad8d72b93
Sean Lin <seanlin>
2021-07-26 16:49:52 +0800

    Initialize project

---
+++ src/main/java/com/seanlindev/springframework/UserAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.seanlindev.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthApiApplication.class, args);
+	}
+
+}
